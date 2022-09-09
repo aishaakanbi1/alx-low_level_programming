3f99bb9760de30f55c5fb14bccf896e7e9151c1b
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-09 14:14:15 -0700

    c2

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers go here */
+#include <stdio.h>
+
+/**
+* main - program execution starts here
+* Description: this the main function
+* Return: an integer to signify success or fail
+*/
+int main(void)
+{
+/tint n;
+
+/tsrand(time(0));
+n = rand() - RAND_MAX / 2;
+
---
+++ 0x01-variables_if_else_while/READme.md
@@ -0,0 +1,1 @@
+this contains if and else programs in c
