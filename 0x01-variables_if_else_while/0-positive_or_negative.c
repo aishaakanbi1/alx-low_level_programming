13a9b0cd0867e8f73c702d9723b016664c9cda04
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-09 14:53:41 -0700

    c2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,27 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 /* more headers goes there */
+#include <stdio.h>
 
-
-
-/* betty style doc for function main goes there */
-
+/** 
+* main - program execution starts here
+* Description: this is the main function
+* Return: an integer to signify successor fail
+*/
 int main(void)
-
 {
+    int n;
 
-int n;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
 
-
-
-srand(time(0));
-
-n = rand() - RAND_MAX / 2;
-
-/* your code goes there */
-
-return (0);
-
+    if (n > 0)
+        printf("%d is positive", n);
+    else if (n < 0)
+        printf("%d is negative", n);
+    else 
+        printf("%d is zero", n);
+ 
+    printf("\n");
+    return (0);
 }
