0a2bc6970319dd74155b96a068186cd5478900eb
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-08 20:38:13 -0700

    c1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
 * main - program execution starts here
-* Description: this is a main function?
-* Return: an integer is retured to signify success or failure
+* Description: this is the main functin
+* Return: returrn an integer to signify success or fail
 */
 int main(void)
 {
-    puts("programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
