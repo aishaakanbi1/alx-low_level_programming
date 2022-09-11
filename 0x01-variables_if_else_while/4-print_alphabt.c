f41833d7485af665db7901b7fa861cbf8407166e
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-11 12:30:23 -0700

    c2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+
+/**
+
+ *main - program execution starts here
+
+ *Description: this is the main function
+
+ *Return: an integer to signify success or fail
+
+ */
+
+int main(void)
+
+{
+
+int alphabet = a;
+
+
+
+while (alphabet <= z)
+
+{
+
+if (alphabet != q  && alphabet != e)
+
+putchar(alphabet);
+
+alphabet++;
+
+}
+
+
+
+putchar(n);
+
+
+
+return (0);
+
+}
