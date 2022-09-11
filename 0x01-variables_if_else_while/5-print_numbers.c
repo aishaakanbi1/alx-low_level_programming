48acfa3944cb1c99b6eb49696f2885d0afc70500
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-11 12:00:38 -0700

    c2

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/** 
+* main - program execution starts here
+* Description: this is the main function
+* Return: an integer to signify sucess or fail
+*/
+int main(void)
+{ 
+         int digit = 0;
+
+         while (digit < 10)
+         {
+                printf("%d", digit);
+                digit++;
+         }
+         putchar(n);
+        
+         return (0);
+}
