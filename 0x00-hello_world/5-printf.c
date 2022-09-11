afdd5090d509a2c167b7745ab350997677952347
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-11 09:58:05 -0700

    c1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - program execution starts here
+* Description: this is the main function
+* Return: return an integer to signify success or fail
+*/
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
