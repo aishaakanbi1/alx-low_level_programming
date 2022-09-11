89d813b59fe99642d595d4b387679a8698639814
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-11 10:31:23 -0700

    c1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - program execution starts here
+* Description: this is the main function
+* Return: an integer to signify success or fail
+*/
+int main(void)
+{
+    fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"), 59, 1, stderr);
+    return (1);
+}
