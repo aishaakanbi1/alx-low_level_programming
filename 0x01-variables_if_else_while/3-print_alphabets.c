83aa32853ac25b154ade7d7b4494b68c1c3d0d8b
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-11 12:15:54 -0700

    c2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - program execution starts here
+*Description: this is the main function
+*Return: an integer to signify success or failure
+*/
+int main(void)
+{
+
+      int alphabet = a;
+
+      while (alphabet < 123)
+      {
+               putchar(alphabet);
+               alphabet++;
+      }
+      
+      alphabet = A;
+      
+      while (alphabet < 91)
+      {
+              putchar(alphabet);
+              alphabet++;
+      }
+      putchar(n);
+
+      return (0);
+}
