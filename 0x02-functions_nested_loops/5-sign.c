f1fffc47e769ee86322067af1d52763a51becd33
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-13 15:05:20 -0700

    c3

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+
+
+/**
+
+ *print_sign - program execution starts here
+
+ *@n: number to be compared for sign
+
+ *Description: this is the main function
+
+ *Return: 1if n is positive,  -1 if n is negative, 0 if n is 0
+
+ */
+
+int print_sign(int n)
+
+{
+
+if (n > 0)
+
+{
+
+_putchar(+);
+
+return (1);
+
+}
+
+else if (n < 0)
+
+{
+
+_putchar(-);
+
+return (-1);
+
+}
+
+else
+
+{
+
+_putchar(0);
+
+return (0);
+
+
+
+}
+
+
+
+}
