578898b015087f7dc59772e56d636d0ad179290a
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-14 10:28:10 +1300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,12 @@
  */
 void print_alphabet(void)
 {
-           char  alphabet = a;
+           char  alphabet = 'a';
        
-           while (alphabet <= z)
+           while (alphabet <= 'z')
            {
                   _putchar(alphabet);
                   alphabet++;
             }
-            _putchar(n);
+            _putchar('\n');
 }
