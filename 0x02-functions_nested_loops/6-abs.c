a258b6a251d0bb8007aa97bcd5ff01904556c2b7
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-13 15:10:49 -0700

    c3

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+
+
+/**
+
+ *_abs - outputs the absolute value of a number
+
+ *@n: number to be checked
+
+ *Description: outputs the absolute value of a number
+
+ *Return: the absolute value of n
+
+ */
+
+int _abs(int n)
+
+{
+
+return (abs(n));
+
+}
