da351a47711ba628b8aed5aaa97c89255965916f
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-14 11:16:31 +1300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,31 +1,16 @@
 #include "main.h"
-
 #include <stdlib.h>
 
-
-
 /**
-
  *print_last_digit - prints the last digit of a fn
-
  *@n: number to get the last digit of
-
  *Description: prints the last digit of a fn (number is in ascii form)
-
  *Return: the last didgit
-
  */
-
 int print_last_digit(int n)
-
 {
+        int last_digit = abs(n % 10);
 
-int last_digit = abs(n % 10);
-
-
-
-_putchar(last_digit + 0);
-
-return (last_digit);
-
+         _putchar(last_digit + 0);
+         return (last_digit);
 }
