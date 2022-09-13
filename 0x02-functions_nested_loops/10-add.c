4be57d49561337b8da363ac1155d3d669871a557
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-13 15:35:49 -0700

    c3

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+
+
+/**
+
+ *add - finds the sum of two numbers.
+
+ *@num1: first number to add
+
+ *@num2: second number to add
+
+ *Description: finds the sum of two numbers
+
+ *Return: an integer(the sum of num1 and num2)
+
+ */
+
+int add(int num1, int num2)
+
+{
+
+return (num1 + num2);
+
+}
