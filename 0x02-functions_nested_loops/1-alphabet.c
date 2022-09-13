0a861d9e5b7eb3cfd971c7cb19c42bf9af7aa35a
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-13 14:23:31 -0700

    c3

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+
+ *print_alphabet - prints the alphabets from a - z
+
+ *Description:prints the lower case letters of the english alphabets from a - z
+
+ */
+
+void print_alphabet(void)
+
+{
+
+char  alphabet = a;
+
+
+
+while (alphabet <= z)
+
+{
+
+_putchar(alphabet);
+
+alphabet++;
+
+}
+
+_putchar(n);
+
+}
