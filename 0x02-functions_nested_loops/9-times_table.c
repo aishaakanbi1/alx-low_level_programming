90c19166abe96ac9265c57eab38498e9034db97c
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-13 15:28:47 -0700

    c3

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+
+
+/**
+
+ *times_table - print the multiplication table
+
+ *Description: prints the 9 times table, starting with 0
+
+ */
+
+void times_table(void)
+
+{
+
+int i, j;
+
+int product, product_x, product_y;
+
+
+
+for (i = 0; i < 10; i++)
+
+{
+
+for (j = 0; j < 10; j++)
+
+{
+
+product = i * j;
+
+product_y = product % 10;
+
+product_x = product / 10;
+
+
+
+if (product_x == 0)
+
+{
+
+if (j != 0)
+
+{
+
+_putchar( );
+
+_putchar( );
+
+}
+
+_putchar(product_y + 0);
+
+}
+
+else
+
+{
+
+_putchar( );
+
+_putchar(product_x + 0);
+
+_putchar(product_y + 0);
+
+}
+
+if (j != 9)
+
+{
+
+_putchar(,);
+
+}
+
+}
+
+_putchar(n);
+
+}
+
+
+
+}
