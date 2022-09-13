2478f5aed57a5ede9f390c532a8550f327b40ea0
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-14 11:11:45 +1300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,23 +1,12 @@
 #include <stdlib.h>
 
-
-
 /**
-
  *_abs - outputs the absolute value of a number
-
  *@n: number to be checked
-
  *Description: outputs the absolute value of a number
-
  *Return: the absolute value of n
-
  */
-
 int _abs(int n)
-
 {
-
-return (abs(n));
-
+          return (abs(n));
 }
