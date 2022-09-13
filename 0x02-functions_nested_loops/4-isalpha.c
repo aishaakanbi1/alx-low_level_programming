f8239d171136da48c4a2108479a9442c73d0423a
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-13 15:00:32 -0700

    c3

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+
+
+/**
+
+ *_isalpha - program execution starts here
+
+ *@c: character to be checked
+
+ *Description: this is the main function
+
+ *Return: an integer to signify success or failure
+
+ */
+
+int _isalpha(int c)
+
+{
+
+int is_lowercase = (c >= a && c <= z);
+
+int is_uppercase = (c >= A && c <= Z);
+
+
+
+return (is_lowercase || is_uppercase);
+
+}
