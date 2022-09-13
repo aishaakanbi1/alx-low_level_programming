fc0ddf204430ee7f3b4a8ac77cdb917dc7093869
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-13 15:14:38 -0700

    c3

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+#include <stdlib.h>
+
+
+
+/**
+
+ *print_last_digit - prints the last digit of a fn
+
+ *@n: number to get the last digit of
+
+ *Description: prints the last digit of a fn (number is in ascii form)
+
+ *Return: the last didgit
+
+ */
+
+int print_last_digit(int n)
+
+{
+
+int last_digit = abs(n % 10);
+
+
+
+_putchar(last_digit + 0);
+
+return (last_digit);
+
+}
