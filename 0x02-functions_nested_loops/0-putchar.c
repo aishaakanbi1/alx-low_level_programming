88eed6a1bc071c99c63f8e6dbfe56778473b63c2
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-13 14:04:17 -0700

    c3

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+/**
+
+ *main - program execution starts here
+
+ *Description: this is the main function
+
+ *Return: an integer to signify success or failure
+
+ */
+
+int main(void)
+
+{
+
+
+
+int _putchar(char c);
+
+
+
+_putchar(_);
+
+_putchar(p);
+
+_putchar(u);
+
+_putchar(t);
+
+_putchar(c);
+
+_putchar(h);
+
+_putchar(a);
+
+_putchar(r);
+
+_putchar(n);
+
+return (0);
+
+}
