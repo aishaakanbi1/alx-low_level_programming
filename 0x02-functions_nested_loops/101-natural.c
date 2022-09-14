79c36171f9387cc11af199b93f2234de22319a15
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-14 05:28:41 -0700

    c3

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+
+ *main -program execution starts here
+
+ *Description: prints all the multiples of 3 and 5 and the sums it.
+
+ *Return: 1 or 0 to signify success or failuer
+
+ */
+
+int main(void)
+
+{
+
+int counter, sum;
+
+
+
+sum = 0;
+
+for (counter = 1; counter < 1024; counter++)
+
+{
+
+if (counter % 3 == 0 || counter % 5 == 0)
+
+{
+
+sum += counter;
+
+}
+
+}
+
+printf("%d\n", sum);
+
+
+
+return (0);
+
+}
