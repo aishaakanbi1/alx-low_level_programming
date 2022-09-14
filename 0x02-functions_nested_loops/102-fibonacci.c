b73f43c6e5819643024098b5f1e722c3c9011d3b
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-14 05:35:50 -0700

    c3

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+
+
+/**
+
+ *main - prints the first 50 Fibonacci numbers
+
+ *Description: prints the first 50 Fibonacci numbers
+
+ *Return: an integer to signify success or failure
+
+ */
+
+int main(void)
+
+{
+
+unsigned long int prevfibonacci = 1;
+
+unsigned long int fibonacci = 2;
+
+int counter = 3;
+
+unsigned long int swap;
+
+
+
+printf("%lu, %lu, ", prevfibonacci, fibonacci);
+
+while (counter <= 50)
+
+{
+
+swap = fibonacci;
+
+fibonacci = fibonacci + prevfibonacci;
+
+if (counter != 50)
+
+printf("%lu, ", fibonacci);
+
+else
+
+printf("%lu", fibonacci);
+
+prevfibonacci = swap;
+
+counter++;
+
+}
+
+putchar(n);
+
+
+
+return (0);
+
+}
