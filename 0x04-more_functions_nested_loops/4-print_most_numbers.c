e6aa0871431e2637053d481ce38e896e5ce7938a
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-16 11:28:30 +1300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,22 @@
-ghjkk
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0-9 in ASCII form
+ * skipping 2 and 4
+ * Description: prints 0-9 in ASCII form
+ * skipping 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int character = '0';
+
+	while (character <= '9')
+	{
+		if (character != '2' && character != '4')
+		{
+			_putchar(character);
+		}
+		character++;
+	}
+	_putchar('\n');
+}
