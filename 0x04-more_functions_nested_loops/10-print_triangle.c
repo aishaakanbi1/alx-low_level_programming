86bb161b577172564ef9cfe3db0677ed4aba3057
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-16 11:37:16 +1300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,24 @@
-bdn
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: the size of the triangle
+ * Description: prints a triangle of hash (#) character
+ */
+void print_triangle(int size)
+{
+	int length, space, body;
+
+	for (length = size; length >= 1; length--)
+	{
+		for (space = 1; space < length; space++)
+		{
+			_putchar(' ');
+		}
+		for (body = -1; body < size - space; body++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size < 1)
+		_putchar('\n');
+}
