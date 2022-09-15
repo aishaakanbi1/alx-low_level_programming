b179b5a588d605ef3ec5059037b605d3d5c2158e
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-16 11:35:04 +1300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,19 @@
-hhjjj
+#include "main.h"
+
+/**
+ * print_line - prints line in the terminal
+ * @n: the number of lines to print
+ * Description: prints n number of lines in the terminal
+ */
+void print_line(int n)
+{
+	int linecount;
+
+	linecount = 0;
+	while (linecount < n)
+	{
+		_putchar('_');
+		linecount++;
+	}
+	_putchar('\n');
+}
