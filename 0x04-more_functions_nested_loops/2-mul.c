5e3f6459825a829cf9561594ad8ab9c136721067
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-16 11:31:34 +1300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,11 @@
-
 /**
- * _isdigit - checks if a character is a digit
- * @c: character to be checked
- * Description: checks if a character is a digit
- * Return: 1 if true and 0 if false
+ * mul - multiplies two digits
+ * @a: 1st digit in the multiplication
+ * @b: 2nd digit to be multiplied
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
  */
-int _isdigit(int c)
+int mul(int a, int b)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
+	return (a * b);
 }
