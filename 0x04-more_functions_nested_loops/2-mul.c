7b66ffc7ad04d4ba6350e04223a3a5d7f6a895a7
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-16 11:19:41 +1300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,14 @@
-iuty
+
+/**
+ * _isdigit - checks if a character is a digit
+ * @c: character to be checked
+ * Description: checks if a character is a digit
+ * Return: 1 if true and 0 if false
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
