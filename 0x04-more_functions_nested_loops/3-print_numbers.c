bc508047f487f59974809a078ea18da11fc1355d
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-16 11:27:57 +1300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,16 @@
-nmjuu
+#include "main.h"
+/**
+ * print_numbers - prints 0-9 in ASCII form
+ * Description: prints 0-9 in ASCII form
+ */
+void print_numbers(void)
+{
+	int character = '0';
+
+	while (character <= '9')
+	{
+		_putchar(character);
+		character++;
+	}
+	_putchar('\n');
+}
