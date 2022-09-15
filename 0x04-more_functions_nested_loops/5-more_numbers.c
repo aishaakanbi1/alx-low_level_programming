2e6fede071e62b1e442133aaa52dca4ce5a490de
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-16 11:29:05 +1300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,25 @@
-bnm
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14 (in ASCII) 10 times
+ * Description: prints 0-14 (in ASCII) 10 times
+ */
+void more_numbers(void)
+{
+	int loopcount, printnumber;
+	int tens, unit;
+
+	for (loopcount = 0; loopcount < 10; loopcount++)
+	{
+		for (printnumber = 0; printnumber <= 14; printnumber++)
+		{
+			tens = printnumber / 10;
+			unit = printnumber % 10;
+			if (tens)
+				_putchar(tens + '0');
+			_putchar(unit + '0');
+		}
+		_putchar('\n');
+	}
+
+}
