1912d3d74942d2fe3559a4fe47c824f81d364e95
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-16 11:36:13 +1300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,23 @@
-bdhjj
+#include "main.h"
+
+/**
+ * print_square - prints a square of hash (#) character
+ * @size: the size of the square
+ * Description: prints a square of hash (#) character
+ */
+void print_square(int size)
+{
+	int length;
+	int breadth;
+
+	for (length = 1; length <= size; length++)
+	{
+		for (breadth = 1; breadth <= size; breadth++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size < 1)
+		_putchar('\n');
+}
