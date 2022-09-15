fc9e27dbbbf4dcfc86e59a0c939a821fe1498eab
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-16 11:36:41 +1300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,38 @@
-vcbdn
+#include <stdio.h>
+
+/**
+ * main - counts from 1 to 100
+ * Description: counts from 1 to 100, printing
+ * Fizz for numbers that are multiples of 3,
+ * Buzz for numbers that are multiples 5,
+ * and FizzBuzz for numbers that are
+ * multiples of both 3 and 5
+ * Return: void
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if (number % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (number % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (number % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+			printf("%d", number);
+
+		if (number != 100)
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
