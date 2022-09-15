e78698f89609d3733271977dc23f473bece465ce
aisha akanbi <adesuwaakanbi60@gmail.com>
2022-09-15 15:03:04 -0700

    c4

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
+int _isupper(int c);
 
-#include <stdio.h>
+int _isdigit(int c);
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+int mul(int a, int b);
 
-#endif /* MAIN_H */
+void print_numbers(void);
+
+int _putchar(char c);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+void print_number(int n);
