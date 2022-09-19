c20d8d756e3570a1019992508646451e009e4c0a
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-20 00:52:45 +1300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,23 @@
-dddg
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: sting to cut in half
+ * Description: prints the second half of a string followed by a new line
+ */
+void puts_half(char *str)
+{
+	int letter_count, midstring;
+
+	/*get length of string*/
+	for (letter_count = 0; str[letter_count] != '\0'; letter_count++)
+		;
+
+	/*get the middle of the string*/
+	midstring = letter_count / 2;
+
+	/*print from middle of sting to the end*/
+	for (letter_count--; midstring <= letter_count; midstring++)
+		_putchar(str[midstring]);
+	_putchar('\n');
+}
