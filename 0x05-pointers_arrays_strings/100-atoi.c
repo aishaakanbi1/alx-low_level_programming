76add1fae978746c73c46a2ce7507a0e63a446b1
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-20 01:10:20 +1300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,39 @@
-ggddy
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer number
+ * @s: String to be converted to an integer number
+ *
+ * Return: Resulting integer number
+ */
+int _atoi(char *s)
+{
+	int i;
+	int sign;
+	unsigned int num;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign = -sign;
+		}
+
+		for (; s[i] >= '0' && s[i] <= '9'; i++)
+		{
+			num = num * 10 + ((int)s[i] - '0');
+		}
+
+		if (num > 0 || s[i] == '\0')
+		{
+			break;
+		}
+
+		i++;
+	}
+
+	return (num * sign);
+}
