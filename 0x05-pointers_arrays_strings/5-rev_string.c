8fedc7d41d516a34b52434f8cd01ae194ec33b30
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-20 00:51:54 +1300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,23 @@
-vdbdb
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: sting to reverse
+ * Description: reverses a string
+ */
+void rev_string(char *s)
+{
+	int letter_count, left_index, swap;
+
+	/*get length of string*/
+	for (letter_count = 0; s[letter_count] != '\0'; letter_count++)
+		;
+	/* swap letters from both ends till you get to the middle*/
+	for (left_index = 0, letter_count--; left_index < letter_count;
+			left_index++, letter_count--)
+	{
+		swap = s[left_index];
+		s[left_index] = s[letter_count];
+		s[letter_count] = swap;
+	}
+}
