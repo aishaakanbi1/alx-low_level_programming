3db0b447eaa1d049b0876d32141232b307b5c621
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-20 00:48:02 +1300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,20 @@
-ahsyy
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to reverse
+ * Description: prints a string in reverse and a new line
+ */
+void print_rev(char *s)
+{
+	int letter_count;
+
+	/*get length of string*/
+	for (letter_count = 0; s[letter_count] != '\0'; letter_count++)
+		;
+	/* print string in reverse */
+	for (letter_count--; letter_count >= 0; letter_count--)
+		_putchar(s[letter_count]);
+	_putchar('\n');
+
+}
