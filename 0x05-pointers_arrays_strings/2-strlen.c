5d79dcbc8128a8bae1032ae0b4b5b1dacf19e633
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-20 00:47:06 +1300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,17 @@
-ssfsf
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - return the length of string
+ * @s: String to find the length
+ * Description: returns the length of a string
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	int letter_count;
+
+	for (letter_count = 0; s[letter_count] != '\0'; letter_count++)
+		;
+	return (letter_count);
+}
