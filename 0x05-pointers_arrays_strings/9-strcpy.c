0837965a70a3500b0649854ecd3026bb647206cc
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-20 00:53:41 +1300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,19 @@
-ddt
+/**
+ * _strcpy - copy source string to a destination sting
+ * @dest: string to copy to
+ * @src: string to copy from
+ * Description: copy's the contents of a source string to a destination string
+ * Return: the destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index;
+	int letter;
+
+	for (index = 0; ((letter = src[index]) != '\0'); index++)
+	{
+		dest[index] = letter;
+	}
+	dest[index] = letter;
+	return (dest);
+}
