530a5499ea00179db039974db73ebd2c18c8ebfa
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-22 02:14:07 +1300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,37 @@
-svdfr
+#include "main.h"
+
+/**
+ * leet - encodes a string as 1337
+ * @str: String argument
+ *
+ * Return: @str
+ */
+char *leet(char *str)
+{
+
+	char *check;
+	char *replace;
+	int i;
+	int j;
+
+	check = "aeotl";
+	replace = "43071";
+	i = 0;
+	while (check[i] != '\0')
+	{
+		j = 0;
+		while (str[j] != '\0')
+		{
+			if (str[j] == check[i] || str[j] == (check[i] - 32))
+			{
+				str[j] = replace[i];
+			}
+
+			j++;
+		}
+
+		i++;
+	}
+
+	return (str);
+}
