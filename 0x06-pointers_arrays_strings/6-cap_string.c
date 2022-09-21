dcd088c6e150f6e43018dc5ead4fa2978cf7a727
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-22 02:12:41 +1300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,43 @@
-dvff
+#include "main.h"
+
+/**
+ * cap_string - Capitalize every word in a string
+ * @str: String argument
+ *
+ * Return: Capitalized @str
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	i = 0;
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] = str[0] - 32;
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		switch (str[i])
+		{
+			case ' ':
+			case '\n':
+			case '\t':
+			case ',':
+			case '.':
+			case ';':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				{
+					str[i + 1] = str[i + 1] - 32;
+				}
+		}
+	}
+
+	return (str);
+}
