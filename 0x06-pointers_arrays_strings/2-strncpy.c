db56773e30f4e911b6793d1c5c0d70f67cbec1c4
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-22 02:02:32 +1300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,27 @@
-2bdg
+#include "main.h"
+
+/**
+ * _strncpy - copies at most n characters from src to dest
+ * @dest: destination
+ * @src: source
+ * @n: number of characters to copy
+ *
+ * Return: @dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
