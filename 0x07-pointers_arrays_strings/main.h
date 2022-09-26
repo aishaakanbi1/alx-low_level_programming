a0a7d8ad306d3c8f99e4f455d83dc1ee96a9b786
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-26 22:18:36 +1300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,1 +1,16 @@
-svfsr
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+int _putchar(char);
+char *_memset(char *, char, unsigned int);
+char *_memcpy(char *, char *, unsigned int);
+char *_strchr(char *, char);
+unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *, int);
+void set_string(char **, char *);
+
+#endif /* MAIN_H_*/
