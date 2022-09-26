c2fad9e87489f1a40a6b62b1fbbe2140b32e7380
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-26 23:40:44 +1300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,27 @@
-scfdr
+#include "main.h"
+
+/**
+ * print_chessboard - prints out the contents of a multidimensional array
+ * @a: multidimensional array
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	/*Declaring Variables*/
+	int i, j;
+
+	i = 0;
+	while (i < 8) /*Repetition*/
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n'); /*Print Newline*/
+		i++; /*i + 1*/
+	}
+}
+
