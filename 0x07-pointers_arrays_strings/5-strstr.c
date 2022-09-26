58cac3398ece8dc0db3e9389357ae11910c676c0
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-26 23:29:50 +1300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,33 @@
-sdd
+#include "main.h"
+
+/**
+ * _strstr - returns the location of a given substring in a string
+ * @haystack: String to search
+ * @needle: Substring to search for with @haystack
+ *
+ * Return: Pointer to @needle within @haystack
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *str1, *str2; /*Declaring Variables*/
+
+	while (*haystack != '\0')
+	{
+		str1 = haystack;  /*Setting Values*/
+		str2 = needle;
+
+		/*While comparism is possible*/
+		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
+		{
+			haystack++;
+			str2++;
+		}
+		
+		if (*str2 == '\0')
+		{
+			return (str1); /*return pointer to location of haystack*/
+		}
+		haystack = str1 + 1;
+	}
+	return (0);
+}
