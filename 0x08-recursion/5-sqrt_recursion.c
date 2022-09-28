2f99a48f8b87bf8b21386c896cf3cbe0ac983efa
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-29 02:32:32 +1300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,1 +1,45 @@
-veffr
+#include "main.h"
+
+int _sqrt_recursion_helper(int, int, int);
+
+/**
+ * _sqrt_recursion - Determines the square root of n
+ * @n: Number to be examined
+ *
+ * Return: Square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_recursion_helper(n, 0, n));
+}
+
+/**
+ * _sqrt_recursion_helper - helps determine the square root of n
+ * @n: Number to be examined
+ * @start: Number to start search
+ * @end: Number to end search
+ *
+ * Return: Square root of n
+ */
+int _sqrt_recursion_helper(int n, int start, int end)
+{
+	long mid;
+
+	if (start > end)
+		return (-1);
+
+	mid = (start + end) / 2;
+
+	if (mid * mid == n)
+	{
+		return ((int)mid);
+	}
+	else if (mid * mid < n)
+	{
+		return ((int)_sqrt_recursion_helper(n, mid + 1, end));
+	}
+	else
+	{
+		return ((int) _sqrt_recursion_helper(n, start, mid - 1));
+	}
+}
