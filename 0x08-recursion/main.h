ed2e56a4195aac72aa576e9c7b51a8bc445d8450
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-09-29 02:22:50 +1300

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,1 +1,14 @@
-dff
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+void _puts_recursion(char *);
+void _print_rev_recursion(char *);
+int _strlen_recursion(char *);
+int factorial(int);
+int _pow_recursion(int, int);
+int _sqrt_recursion(int);
+int is_prime_number(int);
+int is_palindrome(char *);
+
+#endif /*MAIN_H*/
