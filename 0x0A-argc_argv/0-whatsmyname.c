1c2b62dfb2772e3b14b7b555a63106719875e342
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-10-01 05:50:35 +1300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,17 @@
-sfsfrr
+#include <stdio.h>
+
+/**
+ * main - main entry point of the program
+ * @argc: Count of command-line arguments
+ * @argv: Vector containing command-line arguments.
+ *
+ * Description: This program prints it's name to the screen
+ * Return: Always 0 (Successful)
+ */
+int __attribute__((unused)) main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
