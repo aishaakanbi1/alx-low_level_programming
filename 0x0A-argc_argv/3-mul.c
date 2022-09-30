21d56dd3368aea85c4e6dd633a261405b86c95e7
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-10-01 05:56:59 +1300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,30 @@
-ssr
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main entry point of the program
+ * @argc: Count of the command-line arguments
+ * @argv: Vector containing the command-line arguments
+ *
+ * Description: Calculates and prints the product of the command-line
+ * arguments.
+ * Return: Always 0 (Successful)
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", (num1 * num2));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
