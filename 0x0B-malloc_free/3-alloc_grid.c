3397044db65f076cd77f58038412991527d1f3e6
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-10-04 21:02:01 +1300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,1 +1,52 @@
-dfr
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Creates a height x width grid in memory
+ * @width: Width of the grid (columns)
+ * @height: Height of the grid (rows)
+ *
+ * Return: Pointer to the grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i, j;
+
+	if ((height > 0) && (width > 0))
+	{
+		/*Create the Grid*/
+		ptr = (int **) malloc(sizeof(int *) * height);
+		if (ptr == 0)
+		{
+			free(ptr);
+			return (0);
+		}
+		for (i = 0; i < height; i++)
+		{
+			ptr[i] = (int *) malloc(sizeof(int) * width);
+			if (ptr[i] == 0)
+			{
+				for (j = 0; j <= i; j++)
+					free(ptr[j]);
+				free(ptr);
+				return (0);
+			}
+		}
+
+		/*Set all grid points to zero*/
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				ptr[i][j] = 0;
+			}
+
+		}
+	}
+	else
+	{
+		return (0);
+	}
+	return (ptr);
+}
