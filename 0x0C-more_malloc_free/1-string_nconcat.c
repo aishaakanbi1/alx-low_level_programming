48c5c5c7e2c8958e1af7101e4bbcf09bbd071c23
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-10-07 10:52:15 +1300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,1 +1,38 @@
-__attribute__('(''('
+#include "main.h"
+
+/**
+ * string_nconcat - multiplies two digits
+ * @s1: 1st digit in the multiplication
+ * @s2: 1st digit in the multiplication
+ * @n: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int i, j;
+	unsigned int size;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (strlen(s2) < n)
+		n = strlen(s2);
+	size = strlen(s1) + n + 1;
+	ptr = malloc(size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (j = 0; s2[j] != '\0' && j < n; j++, i++)
+	{
+		ptr[i] = s2[j];
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
