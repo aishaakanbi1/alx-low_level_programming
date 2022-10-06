b55f713bffbc233396c44a4e12fb90d810a637c2
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-10-07 10:59:14 +1300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,25 @@
-dftr
+#include "main.h"
+
+/**
+ * _calloc - multiplies two digits
+ * @nmemb: 1st digit in the multiplication
+ * @size: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned long int all_size, i;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	all_size = nmemb * size;
+	ptr = malloc(all_size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < all_size; i++)
+		ptr[i] = '\0';
+	return (ptr);
+}
