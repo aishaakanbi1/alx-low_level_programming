94789a8d6d9f8df816331da26c3ac94d9ecaf23d
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-10-11 06:55:49 +1300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,14 @@
-fhy
+#include <stdio.h>
+
+/**
+ * main - main entry point of the program
+ *
+ * Description: prints the name of the current file to the screen
+ * followed by a newline
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
