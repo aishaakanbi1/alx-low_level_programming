afe144d41be6c29eabafa59ee253445188d3f4a6
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-10-11 07:09:58 +1300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,35 @@
-ette
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - dog data type
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Dog's owner's name
+ *
+ * Description: Data type that can be used to represent a dog
+ * in out code.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef of struct dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Description: alias for type struct dog
+ */
+typedef struct dog dog_t;
+
+/* Function Declarations */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
