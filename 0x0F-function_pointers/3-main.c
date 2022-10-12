0c474b18c389df2c65ff595746eb422684fb1b81
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-10-13 11:36:11 +1300

    Create 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+
+/**
+ * main - main entry point of the program
+ * @argc: count of command line arguments
+ * @argv: vector containing command line arguments
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	char operator;
+	int (*f)(int, int); /*pointer to a function*/
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	f = get_op_func(argv[2]);
+	if (f == NULL) /*Operator not found*/
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operator = *argv[2];
+	if ((operator == '/' || operator == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = f(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
