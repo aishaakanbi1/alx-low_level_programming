3c94388de52dedff69751de39888c7f9ebe60c13
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-10-13 11:22:04 +1300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,16 @@
-ase
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints out a name to the screen
+ * @name: string parameter (Name)
+ * @f: pointer to a function that takes a string argument and returns nothing
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	f(name); /*Calls the function with the string*/
+}
