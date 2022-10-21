b0550e51a482d0ffdfa280096dcabc834370fae7
Aisha Ebunoluwa Akanbi <110837578+aishaakanbi1@users.noreply.github.com>
2022-10-22 03:44:50 +1300

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,1 +1,22 @@
-etyu
+#include "lists.h"
+
+/**
+ * free_list - frees up all the memory allocated for a list
+ * @head: points to a node on the list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		temp = head;
+		free(head->str);
+		head = head->next;
+		free(temp);
+	}
+}
