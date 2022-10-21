44fe239863f61baadd65d00255510d478abf8563
Aisha Ebunoluwa Akanbi <110837578+aishaakanbi1@users.noreply.github.com>
2022-10-22 03:42:12 +1300

    Create lists.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,34 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/* Function Declarations */
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+/* Advanced Tasks function declaration */
+void print_first(void) __attribute__ ((constructor));
+
+#endif /* LISTS_H_ */
