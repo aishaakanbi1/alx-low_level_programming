0d236d99d8ced1bd1b218e198ef9716d4928ef37
Aisha Ebunoluwa Akanbi <110837578+aishaakanbi1@users.noreply.github.com>
2022-10-22 03:48:57 +1300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,14 @@
-ety
+#include "lists.h"
+
+/**
+ * print_first - function to be executed before main()
+ *
+ * Return: void
+ */
+void print_first(void)
+{
+	char *message1 = "You're beat! and yet, you must allow,\n";
+	char *message2 = "I bore my house upon my back!\n";
+
+	printf("%s%s", message1, message2);
+}
