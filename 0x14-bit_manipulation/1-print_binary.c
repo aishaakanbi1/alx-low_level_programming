53866fe9ca0454537b088c95cada8ec4a0f159ed
Aisha Ebunoluwa Akanbi <110837578+aishaakanbi1@users.noreply.github.com>
2022-10-28 01:10:46 +1300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,26 @@
-ryuu
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal to the screen
+ * @num: number to convert to binary
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int num)
+{
+	int temp;
+	static int count;
+
+	if (num == 0 && count > 0)
+		return;
+	else if (num == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	temp = (num & 1);
+	count++;
+	print_binary(num >>= 1);
+	_putchar('0' + temp);
+}
