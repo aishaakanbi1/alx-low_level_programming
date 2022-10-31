97ac6ba2e8e64482dbb8287e5b1921f08ccf3723
Aisha Ebunoluwa Akanbi <110837578+aishaakanbi1@users.noreply.github.com>
2022-11-01 01:32:09 +1300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,1 +1,50 @@
-ty
+#include "main.h"
+
+/**
+ * main - main entry point of the program
+ * @ac: count of command line arguments
+ * @av: vector holding command line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int ac, char *av[])
+{
+	int file_from, file_to, length = 1;
+	char buffer[1024];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		exit(97);
+	}
+	file_from = open(av[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	while (length)
+	{
+		length = read(file_from, buffer, 1024);
+
+		if (write(file_to, buffer, length) != length)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+
+	if (close(file_from) == -1 || close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+
+}
